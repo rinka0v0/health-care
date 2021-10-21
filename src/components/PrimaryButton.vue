125e2477235e192fe4e35b3f406ecc4f5d7279c7
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-10-21 12:43:19 +0900

    ボタンコンポーネントの作成
    とりあえず最低限の機能とデザイン

---
+++ src/components/PrimaryButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="btn">
+    <a href="#" v-on:click="onClick">
+      {{ value }}
+    </a>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {
+    value: String,
+    onClick: Function,
+  },
+})
+export default class PrimaryButton extends Vue {
+  value!: string;
+  onClick!: () => void;
+}
+</script>
+
+<style>
+.btn a {
+  padding: 8px;
+  text-decoration: none;
+  background: #d4e2d4;
+  min-width: 100px;
+  border-radius: 1em;
+  user-select: none;
+}
+.btn a {
+  color: inherit;
+  border: none;
+}
+.btn:hover {
+  opacity: 0.7;
+}
+</style>
