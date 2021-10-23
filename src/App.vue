117e63c96d5c7c8322f4cc85c26c744640dec440
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-10-23 14:55:16 +0900

    colorとbackgroundColorをグローバルに設定した

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="app">
+    <router-view />
   </div>
-  <router-view />
 </template>
 
 <style lang="scss">
@@ -12,19 +10,13 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #df7861;
+  min-height: 100vh;
+  background-color: #fcf8e8;
+  margin: 0;
+  padding: 0;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.app {
+  background-color: #fcf8e8;
 }
 </style>
