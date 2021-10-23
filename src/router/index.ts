62fed0f0c42af737d18da3caa0959feaa101645f
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-10-23 13:36:27 +0900

    ルーティングの設定

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,21 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Home from "../views/Home.vue";
+import Index from "../views/Index.vue";
+import Setting from "../views/Setting.vue";
+import Detail from "../views/Detail.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "Index",
+    component: Index,
   },
+  { path: "/home", name: "home", component: Home },
+  { path: "/detail", name: "home", component: Detail },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/setting",
+    name: "setting",
+    component: Setting,
   },
 ];
 
