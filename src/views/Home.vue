0bac346ec5088a9a50bfbbbd95517ca7b0662ec2
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-10-25 23:07:15 +0900

    [update] カレンダーコンポーネントを表示させた

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <Header />
+  <Calendar />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
+import Header from "@/components/Header.vue";
+import Calendar from "@/components/Calendar.vue";
 @Options({
   components: {
-    HelloWorld,
+    Header,
+    Calendar,
   },
 })
 export default class Home extends Vue {}
