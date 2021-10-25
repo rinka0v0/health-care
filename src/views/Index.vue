fca79393518114da5e1e70b47301d16bb9c23b0f
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-10-25 23:04:05 +0900

    [add] Indexページの作成

---
+++ src/views/Index.vue
@@ -0,0 +1,33 @@
+<template>
+  <Header />
+  <h1 class="title">体調管理アプリ</h1>
+  <p>毎日の体調を管理することができます。</p>
+  <p>googleアカウントでログインが可能です</p>
+  <PrimaryButton value="Sign in with Google" :onClick="handleClick" />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Header from "@/components/Header.vue";
+import PrimaryButton from "@/components/PrimaryButton.vue";
+
+@Options({
+  components: {
+    Header,
+    PrimaryButton,
+  },
+})
+export default class Index extends Vue {
+  count = 0;
+  handleClick = (): void => {
+    // ログイン処理をここに書く
+    console.log("hello");
+  };
+}
+</script>
+
+<style>
+.title {
+  margin: 0;
+}
+</style>
