5f46638e43213f5e47baa26267ec591040d63469
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-10-25 23:06:16 +0900

    [add] Headerコンポーネントの追加
    とりあえず作っただけ

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="header">
+    <div>header</div>
+  </div>
+</template>
+
+<script>
+import { Vue } from "vue-class-component";
+
+export default class Header extends Vue {}
+</script>
+
+<style>
+.header {
+  height: 60px;
+  /* position: fixed; */
+  /* top: 0; */
+}
+.header div {
+  color: #df7861;
+}
+</style>
