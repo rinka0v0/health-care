90624062de84253ba629cd494018e504e6b04333
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-10-29 23:42:18 +0900

    [update] 適切なページへのリダイレクト処理の追加
    ログイン状態をみてルーティングを行なっている

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,8 @@ import Home from "../views/Home.vue";
 import Index from "../views/Index.vue";
 import Setting from "../views/Setting.vue";
 import Detail from "../views/Detail.vue";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../../firebase";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -10,18 +12,50 @@ const routes: Array<RouteRecordRaw> = [
     name: "Index",
     component: Index,
   },
-  { path: "/home", name: "home", component: Home },
-  { path: "/:date", name: "detail", component: Detail },
+  { path: "/home", name: "home", component: Home, meta: { requireAuth: true } },
+  {
+    path: "/:date",
+    name: "detail",
+    component: Detail,
+    meta: { requireAuth: true },
+  },
   {
     path: "/:date/setting",
     name: "setting",
     component: Setting,
+    meta: { requireAuth: true },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some((record) => record.meta.requireAuth);
+  console.log(requiresAuth);
+
+  if (requiresAuth) {
+    onAuthStateChanged(auth, (user) => {
+      console.log(user, "login");
+      if (user) {
+        next();
+      } else {
+        next({ path: "/" });
+      }
+    });
+  } else {
+    onAuthStateChanged(auth, (user) => {
+      console.log(user, "logout");
+
+      if (user) {
+        next({ path: "/home" });
+      } else {
+        next();
+      }
+    });
+  }
 });
 
 export default router;
