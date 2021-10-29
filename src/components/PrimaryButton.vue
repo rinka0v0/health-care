1494195f74a0e3731c888695b56d8a66444ac453
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-10-29 15:58:00 +0900

    [update] ボタンをdivではなくbuttonタグで作り直した

--- src/components/PrimaryButton.vue
+++ src/components/PrimaryButton.vue
@@ -1,41 +1,33 @@
 <template>
-  <div class="btn">
-    <a href="#" v-on:click="onClick">
-      {{ value }}
-    </a>
+  <div>
+    <button @click="onClick" class="btn">{{ value }}</button>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-
 @Options({
   props: {
     value: String,
     onClick: Function,
   },
 })
-export default class PrimaryButton extends Vue {
-  value!: string;
-  onClick!: () => void;
-}
+export default class TestButton extends Vue {}
 </script>
 
-<style>
-.btn a {
-  display: block;
-  padding: 8px;
+<style lang="scss" scoped>
+.btn {
+  padding: 0.4em 0.5em;
   text-decoration: none;
   background: #d4e2d4;
   min-width: 100px;
-  border-radius: 1em;
+  border-radius: 0.5em;
   user-select: none;
-}
-.btn a {
   color: inherit;
   border: none;
-}
-.btn:hover {
-  opacity: 0.7;
+  font-size: 1em;
+  &:hover {
+    opacity: 0.7;
+  }
 }
 </style>
