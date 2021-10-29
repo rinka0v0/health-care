5aa2512c1a59f34c271319b947517d02319a02d8
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-10-29 23:43:36 +0900

    [update] ヘッダーコンポーネントの更新
    ログアウト機能をつけた

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,47 @@
 <template>
   <div class="header">
-    <div>header</div>
+    <h3>HealthCare</h3>
+    <PrimaryButton
+      value="SingOut"
+      :onClick="handleClick"
+      class="primary-button"
+    />
   </div>
 </template>
 
-<script>
-import { Vue } from "vue-class-component";
+<script lang="ts">
+import PrimaryButton from "@/components/PrimaryButton.vue";
+import { signOut } from "firebase/auth";
+import router from "@/router";
+import { auth } from "../../firebase";
+import { NavigationFailure } from "vue-router";
 
-export default class Header extends Vue {}
+export default {
+  components: {
+    PrimaryButton,
+  },
+  methods: {
+    async handleClick(): Promise<void | NavigationFailure | undefined> {
+      await signOut(auth);
+      return router.push("/");
+    },
+  },
+};
 </script>
 
-<style>
+<style scoped lang="scss">
 .header {
   height: 60px;
-  /* position: fixed; */
-  /* top: 0; */
-}
-.header div {
-  color: #df7861;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #ecb390;
+  h3 {
+    color: #df7861;
+    margin-left: 1em;
+  }
+  .primary-button {
+    margin: 1em;
+  }
 }
 </style>
