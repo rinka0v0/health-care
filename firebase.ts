cabd63959a3bc3173e8e6d794150c6d35909947e
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-10-30 10:05:35 +0900

    [fix] バグの修正
    firebaseConfigのタイポを修正

--- firebase.ts
+++ firebase.ts
@@ -1,3 +1,4 @@
+import router from "@/router";
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -5,16 +6,24 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: process.env.VUE_APP_API_KEY,
   authDomain: process.env.VUE_APP_AUTH_DOMAIN,
-  profectId: process.env.VUE_APP_PROJECTID,
+  projectId: process.env.VUE_APP_PROJECTID,
   appId: process.env.VUE_APP_APP_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_SENDER_ID,
 };
 
 initializeApp(firebaseConfig);
 
 export const auth = getAuth();
-// export const db = getFirestore();
+export const db = getFirestore();
 
 const provider = new GoogleAuthProvider();
 export const signInWithGoogle = (): void => {
-  signInWithRedirect(auth, provider);
+  signInWithRedirect(auth, provider)
+    .then(() => {
+      router.push("/home");
+    })
+    .catch((e) => {
+      console.log(e.message);
+    });
 };
