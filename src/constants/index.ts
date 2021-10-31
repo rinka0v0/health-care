70dbe00de91a50b4ad50e235c12fd15702bf9968
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-11-01 00:16:14 +0900

    [add] 定数をまとめておくファイルの作成

---
+++ src/constants/index.ts
@@ -0,0 +1,20 @@
+export const conditions = [
+  { label: "良い", value: 0 },
+  { label: "普通", value: 1 },
+  { label: "悪い", value: 2 },
+];
+
+export const headaches = [
+  { label: "激痛", value: 0 },
+  { label: "痛い", value: 1 },
+  { label: "少し痛い", value: 2 },
+];
+export const dizzys = [
+  { label: "ふわふわ", value: 0 },
+  { label: "ぐるぐる", value: 1 },
+];
+export const lowerBackPains = [
+  { label: "激痛", value: 0 },
+  { label: "痛い", value: 1 },
+  { label: "少し痛い", value: 2 },
+];
