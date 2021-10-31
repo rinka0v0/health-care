b09443b77ecfbf3ba088ccbd47f7431501522f18
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-11-01 00:37:34 +0900

    [add] メモコンポーネントの作成

---
+++ src/components/Memo.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="memo-item">
+    {{ content }}
+    <div>{{ date }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: ["content", "createdAt"],
+  computed: {
+    date(): string {
+      const result = this.createdAt.split("-");
+      return result[0] + "年" + result[1] + "月" + result[2] + "日";
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.memo-item {
+  width: 100%;
+  max-width: 300px;
+  border: 1px solid #ddd;
+  padding: 1em;
+  text-align: left;
+  margin: 0 auto;
+  div {
+    text-align: right;
+    margin-top: 1em;
+  }
+}
+</style>
