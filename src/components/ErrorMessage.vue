0ea66c57058d931bc1b64f625445d8d965139fe0
rinka0v0 <rinka0c0@gmail.com>
2022-02-02 22:20:41 +0900

    エラーメッセージを表示するコンポーネントの作成

---
+++ src/components/ErrorMessage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div :class="$style.errorMessage">{{ message }}</div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: ["message"],
+})
+export default class ErrorMessage extends Vue {}
+</script>
