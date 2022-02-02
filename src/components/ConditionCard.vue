69c61f013b6718c62d95ba868a45913b6ec462af
rinka0v0 <rinka0c0@gmail.com>
2022-02-02 17:23:30 +0900

    記録を表示するコンポーネントの作成

---
+++ src/components/ConditionCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div :class="$style.wrapper">
+    <div :class="$style.label">{{ label }}</div>
+    <div :class="$style.content">{{ contentData }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: ["label", "contentData"],
+  created() {
+    console.log(this.label, this.contentData);
+  },
+})
+export default class ConditionCard extends Vue {}
+</script>
+
+<style lang="scss" module>
+.wrapper {
+  display: flex;
+  justify-content: space-between;
+  //   align-items: center;
+  height: 3rem;
+  line-height: 3rem;
+  vertical-align: middle;
+  width: 100%;
+}
+</style>
