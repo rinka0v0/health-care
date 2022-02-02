98589cebad932f5b79553ad401bd9fbd5c6cf725
rinka0v0 <rinka0c0@gmail.com>
2022-02-02 17:24:39 +0900

    ラベル一覧の定数を追加

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,21 @@
+export const conditionsFace = [
+  { label: null, value: 0 },
+  { label: null, value: 1 },
+  { label: "😵", value: 2 },
+];
+
+export const HEALTH_LABELS = {
+  temperature: "体温",
+  headaches: "頭痛",
+  lowerBackPains: "腰痛",
+  dizzies: "めまい",
+  condition: "体調",
+};
+
+export const HEALTH_INFO_KEYS = Object.keys(HEALTH_LABELS).map((key) => {
+  return key;
+});
+
 export const conditions = [
   { label: "良い", value: 0 },
   { label: "普通", value: 1 },
@@ -9,7 +27,7 @@ export const headaches = [
   { label: "痛い", value: 1 },
   { label: "少し痛い", value: 2 },
 ];
-export const dizzys = [
+export const dizzies = [
   { label: "ふわふわ", value: 0 },
   { label: "ぐるぐる", value: 1 },
 ];
