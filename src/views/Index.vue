7db37217550c61ea0e518a8d127f7ba7ab8e64ef
rinka0v0 <rinka0c0@gmail.com>
2022-02-03 17:08:47 +0900

    Indexページにローディングの追加

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,26 +1,43 @@
 <template>
-  <h1 class="title">体調管理アプリ</h1>
-  <p>毎日の体調を管理することができます。</p>
-  <p>googleアカウントでログインが可能です</p>
-  <PrimaryButton value="Sign in with Google" :onClick="handleClick" />
+  <div v-if="!load">
+    <h1 class="title">体調管理アプリ</h1>
+    <p>毎日の体調を管理することができます。</p>
+    <p>googleアカウントでログインが可能です</p>
+    <PrimaryButton value="Sign in with Google" :onClick="handleClick" />
+  </div>
+
+  <div v-else>
+    <Loading />
+  </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import PrimaryButton from "@/components/PrimaryButton.vue";
 import { signInWithGoogle } from "@/utils/auth/index";
+import Loading from "@/components/Loading.vue";
 
 @Options({
+  data() {
+    return {
+      loading: false,
+    };
+  },
   components: {
     PrimaryButton,
+    Loading,
+  },
+  methods: {
+    async handleClick(): Promise<void> {
+      try {
+        await signInWithGoogle();
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
 })
-export default class Index extends Vue {
-  count = 0;
-  handleClick = async (): Promise<void> => {
-    await signInWithGoogle();
-  };
-}
+export default class Index extends Vue {}
 </script>
 
 <style>
