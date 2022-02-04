d1650b69a896b65da828e4e3fa03b33974685a59
rinka0v0 <rinka0c0@gmail.com>
2022-02-04 20:08:39 +0900

    ホーム画面にローディングの追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,39 @@
 <template>
-  <Header />
-  <Calendar />
+  <div v-if="loading">
+    <Loading />
+  </div>
+  <div v-else>
+    <Header />
+    <Calendar />
+  </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import Header from "@/components/Header.vue";
 import Calendar from "@/components/Calendar.vue";
+import { defaultConditions, fetchConditions } from "@/utils/fetcher/firestore";
+import Loading from "@/components/Loading.vue";
+
 @Options({
+  data() {
+    return {
+      conditions: defaultConditions,
+      loading: true,
+    };
+  },
+  async created() {
+    // const data = fetchConditions("useId", "2022", "1");
+    // this.conditions = data;
+    this.loading = false;
+  },
+  beforeCreate() {
+    console.log("before");
+  },
   components: {
     Header,
     Calendar,
+    Loading,
   },
 })
 export default class Home extends Vue {}
